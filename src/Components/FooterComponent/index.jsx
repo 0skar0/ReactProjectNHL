d92eb395b523c8d152706eeda6d5c122cd4e83d7
maccydrums <marcusprivat@live.com>
2019-04-23 16:14:33 +0200

    testing

--- src/Components/FooterComponent/index.jsx
+++ src/Components/FooterComponent/index.jsx
@@ -1,1 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styles from './FooterComponent.module.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <p>working</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
