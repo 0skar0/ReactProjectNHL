f327cb9e85735e31b77a01730e9794a9cf2670ee
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2019-04-24 09:19:33 +0200

    Final commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { render } from 'react-dom';
+import thunk from 'redux-thunk';
+import reducer from './Redux/Reducer';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+/* eslint-enable */
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
