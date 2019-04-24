d115450c8ce9b0ebe2241081866dfad6c94b7f58
Oskar Olsson <oskarolsson86@gmail.com>
2019-04-24 10:04:34 +0200

    edited app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import NavbarComponent from './Components/NavbarComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavbarComponent />
-      </div>
+      <div className="App" />
     );
   }
 }
