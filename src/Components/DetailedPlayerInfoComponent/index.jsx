8d3c0bb55cc3da8d2a2de34ac2b809d7b48f6d3c
Oskar Olsson <oskarolsson86@gmail.com>
2019-05-02 15:38:51 +0200

    -

--- src/Components/DetailedPlayerInfoComponent/index.jsx
+++ src/Components/DetailedPlayerInfoComponent/index.jsx
@@ -53,30 +53,51 @@ class DetailedPlayerInfoComponent extends Component {
 
     reverseStatsArray.sort(compare);
     const goaltender = detailedPlayerInfo.people[0].primaryPosition.type;
-    console.log(detailedPlayerInfo.people[0].primaryPosition.type);
+    console.log(detailedPlayerInfo.people[0]);
+    console.log(goaltender);
     return (
       <div className={styles.tableWrapper}>
         <h5>Statistik</h5>
         <Table striped borderless hover variant="dark">
-          <thead>
-            <tr className={styles.borderBottom}>
-              <th>Säsong</th>
-              <th>Lag</th>
-              <th>Liga</th>
-              <th
-                data-toggle="tooltip"
-                data-placement="top"
-                title="Utvisningsminuter"
-              >
-              UM
-              </th>
-              <th>+/-</th>
-              <th>Matcher</th>
-              <th>Mål</th>
-              <th>Assist</th>
-              <th>Poäng</th>
-            </tr>
+
+          <thead className="sticky-top">
+            {goaltender === 'Goalie'
+              ? (
+                <tr className="sticky-top">
+                  <th>Säsong</th>
+                  <th>Lag</th>
+                  <th>Liga</th>
+                  <th>Matcher</th>
+                  <th>Insläppta mål</th>
+                  <th>Räddningsprocent</th>
+                  <th>Räddningar</th>
+                  <th>Antal skott</th>
+                  <th>Hållna nollor</th>
+                </tr>
+              )
+              : (
+                <tr className={styles.borderBottom}>
+                  <th className="sticky-top">Säsong</th>
+                  <th className="sticky-top">Lag</th>
+                  <th className="sticky-top">Liga</th>
+                  <th
+                    className="sticky-top"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title="Utvisningsminuter"
+                  >
+                  UM
+                  </th>
+                  <th className="sticky-top">+/-</th>
+                  <th className="sticky-top">Matcher</th>
+                  <th className="sticky-top">Mål</th>
+                  <th className="sticky-top">Assist</th>
+                  <th className="sticky-top">Poäng</th>
+                </tr>
+              )
+          }
           </thead>
+
           <tbody>
             {detailedPlayerInfo.people[0].stats[0].splits.map((player, i) => (
               <tr key={i} className={styles.borderBottom}>
