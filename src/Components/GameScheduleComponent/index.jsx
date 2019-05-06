1d1cfffec116accb6a5f125c0e4f8afb57d0c1e8
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2019-05-06 14:37:00 +0200

    Final commit

--- src/Components/GameScheduleComponent/index.jsx
+++ src/Components/GameScheduleComponent/index.jsx
@@ -4,17 +4,18 @@ import axios from 'axios';
 import styles from './GameScheduleComponent.module.css';
 
 class GameScheduleComponent extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
+      data: [],
+      dates: [],
     };
-
     this.settings = {
-      dots: false,
+      dots: true,
       infinite: false,
       speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToShow: 5,
+      slidesToScroll: 5,
       arrows: false,
       responsive: [
         {
@@ -48,54 +49,65 @@ class GameScheduleComponent extends Component {
     const days = new Date();
     days.setDate(days.getDate() - 5);
     const resDays = days.toISOString().slice(0, 10).replace(/-/g, '-');
-    console.log(res);
-    console.log(resDays);
+    // console.log(res);
+    // console.log(resDays);
     axios.get(`https://statsapi.web.nhl.com/api/v1/schedule?startDate=${resDays}&endDate=${res}`)
-      .then((res) => {
+      .then((response) => {
+        const items = response.data.dates.map(dat => dat);
+        const results = items.reverse();
+        const dates = [];
+        let counter = 0;
+        for (let i = 0; i < results.length; i += 1) {
+          for (let x = 0; x < results[i].games.length; x += 1) {
+            if (results[i].games[x].status.abstractGameState === 'Final') {
+              if (counter < 10) {
+                dates.push(results[i].games[x]);
+                counter += 1;
+              }
+            }
+          }
+        }
         this.setState({
-          dates: res.data,
-          home: res.data.dates[0].games[0].teams.home.team.name,
-          homeID: res.data.dates[0].games[0].teams.home.team.id,
-          away: res.data.dates[0].games[0].teams.away.team.name,
-          awayID: res.data.dates[0].games[0].teams.away.team.id,
-          // teamID: res.data.dates[0].games[1].teams.home.team.id,
+          dates,
         });
       });
     axios.get('https://statsapi.web.nhl.com/api/v1/teams/')
-      .then((res) => {
+      .then((response) => {
         this.setState({
-          data: res.data.teams,
+          data: response.data.teams,
         });
       });
   }
 
+  getAbbrevation = (teamID) => {
+    for (let x = 0; x < this.state.data.length; x += 1) {
+      if (teamID === this.state.data[x].id) {
+        return this.state.data[x].abbreviation;
+      }
+    }
+    return false;
+  }
+
+  getLogo = teamID => `https://www-league.nhlstatic.com/nhl.com/builds/site-core/a2d98717aeb7d8dfe2694701e13bd3922887b1f2_1542226749/images/logos/team/current/team-${teamID}-dark.svg`
+
   render() {
-    const {
-      dates, home, away, homeID,
-    } = this.state;
-
-
-    console.log(dates);
-    const teamLogo = `https://www-league.nhlstatic.com/nhl.com/builds/site-core/a2d98717aeb7d8dfe2694701e13bd3922887b1f2_1542226749/images/logos/team/current/team-${homeID}-dark.svg`;
     return (
-      <div className={styles.container}>
-        <Slider {...this.settings}>
-          <div>
-            <div className={styles.card}>
-              <div className={styles.cardbox}>
-                <img src={teamLogo} alt="logo" />
-                <h4>{home}</h4>
-                <h4>-</h4>
-              </div>
-              <div className={styles.cardbox}>
-                <img src={teamLogo} alt="logo" />
-                <h4>{away}</h4>
-                <h4>-</h4>
-              </div>
+      <Slider {...this.settings}>
+        {this.state.dates.map((team, i) => (
+          <div key={i} className={styles.card}>
+            <div className={styles.cardbox}>
+              <img src={this.getLogo(team.teams.home.team.id)} alt="logo" />
+              <h4>{this.getAbbrevation(team.teams.home.team.id)}</h4>
+              <h4 className={styles.score}>{team.teams.home.score}</h4>
+            </div>
+            <div className={styles.cardbox}>
+              <img src={this.getLogo(team.teams.away.team.id)} alt="logo" />
+              <h4>{this.getAbbrevation(team.teams.away.team.id)}</h4>
+              <h4 className={styles.score}>{team.teams.away.score}</h4>
             </div>
           </div>
-        </Slider>
-      </div>
+        ))}
+      </Slider>
     );
   }
 }
