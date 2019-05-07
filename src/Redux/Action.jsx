14b6bf8c0a720d2126ff8a09cc0546fd33069650
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2019-05-07 14:26:38 +0200

    LatestResultsComponent

--- src/Redux/Action.jsx
+++ src/Redux/Action.jsx
@@ -7,6 +7,12 @@ export const addPlayers = items => ({
 });
 
 // Action creator that returns an action object
+export const addGames = items => ({
+  type: 'ADD_GAMES',
+  games: items,
+});
+
+// Action creator that returns an action object
 export const fetchLoading = bool => ({
   type: 'FETCH_LOADING',
   loading: bool,
@@ -35,6 +41,35 @@ export const fetchPlayers = url => (dispatch) => {
         }
       }
       dispatch(addPlayers(swePlayers));
+      dispatch(fetchLoading(false));
+    })
+    /* eslint-disable no-unused-vars */
+    .catch((error) => {
+      /* eslint-enable no-unused-vars */
+      dispatch(fetchError(true));
+    });
+};
+
+// Using redux-thunk we can make our action creator return a
+// function instead of an action object. This allows us to make asynchronous
+// calls that we will dispatch the relevant data to our redux store when it is ready.
+export const fetchGames = url => (dispatch) => {
+  dispatch(fetchLoading(true));
+  axios.get(url)
+    .then((response) => {
+      const items = response.data.dates.map(dat => dat);
+      const results = items.reverse();
+      const games = [];
+      let counter = 0;
+      for (let i = 0; i < results.length; i += 1) {
+        for (let x = 0; x < results[i].games.length; x += 1) {
+          if (counter < 30) {
+            games.push(results[i].games[x]);
+            counter += 1;
+          }
+        }
+      }
+      dispatch(addGames(games));
       dispatch(fetchLoading(false));
     })
     /* eslint-disable no-unused-vars */
